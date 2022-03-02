a56a8c0a29d4fce29fb44ad110b84e0554a039c0
WiiruxCruz <hialf.92@gmail.com>
2022-03-02 00:57:42 -0600

    Creación de componente selector

--- src/app/paises/paises.module.ts
+++ src/app/paises/paises.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { PaisesRoutingModule } from './paises-routing.module';
 import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [SelectorPageComponent],
   imports: [
     CommonModule,
-    PaisesRoutingModule
+    PaisesRoutingModule,
+	ReactiveFormsModule
   ]
 })
 export class PaisesModule { }
