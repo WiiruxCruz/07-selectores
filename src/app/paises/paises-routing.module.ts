c057858765d4f1534eba09f1720f65cf89972e2b
WiiruxCruz <hialf.92@gmail.com>
2022-03-02 00:18:36 -0600

    Estructura de directorios

---
+++ src/app/paises/paises-routing.module.ts
@@ -0,0 +1,19 @@
+import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+	{
+		path: '',
+		children: [
+			{ path: 'selector', component: SelectorPageComponent },
+			{ path: '**', redirectTo: 'selector' }
+		]
+	}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PaisesRoutingModule { }
