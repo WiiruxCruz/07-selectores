2d76f8b0dd9e746b25ee328dff6d2f7e0d39e5ca
WiiruxCruz <hialf.92@gmail.com>
2022-03-04 01:37:58 -0600

    Tercer selector anidado en progreso

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { PaisSmall } from '../../interfaces/paises.interface';
+import { Pais, PaisSmall } from '../../interfaces/paises.interface';
 import { PaisesService } from '../../services/paises.service';
 import { switchMap, tap } from 'rxjs/operators';
 
@@ -14,10 +14,12 @@ export class SelectorPageComponent implements OnInit {
 
 	miFormulario: FormGroup = this.fb.group({
 		region: [ '', Validators.required ],
-		pais: [ '', Validators.required ]
+		pais: [ '', Validators.required ],
+		frontera: [ '', Validators.required ]
 	})
 
 	paises: PaisSmall[] = [];
+	fronteras: Pais[] = [];
 
 	//lenar selectores
 	regiones: string[] = [];
@@ -50,7 +52,8 @@ export class SelectorPageComponent implements OnInit {
 		this.miFormulario.get('region')?.valueChanges
 		.pipe(
 			tap( ( _ ) => {
-				this.miFormulario.get('pais')?.reset('');
+					this.miFormulario.get('pais')?.reset('');
+					//this.miFormulario.get('frontera')?.reset('');
 				}
 			),
 			switchMap( region => this.paisesService.getPaisesPorRegion( region ) )
@@ -58,7 +61,25 @@ export class SelectorPageComponent implements OnInit {
 		.subscribe( paises => {
 			//console.log(paises);
 			this.paises = paises;
-		})
+		});
+
+
+		//Pais
+		this.miFormulario.get('pais')?.valueChanges
+		/*
+		.pipe(
+			tap( ( _ ) => {
+					this.miFormulario.get('frontera')?.reset('');
+				}
+			),
+			switchMap( pais => this.paisesService.getPaisPorCodigo( pais ) )
+
+		)
+		*/
+		.subscribe( pais => {
+			console.log(pais);
+		});
+
 
 
 	}
--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { PaisSmall } from '../interfaces/paises.interface';
-import { Observable } from 'rxjs'
+import { Pais, PaisSmall } from '../interfaces/paises.interface';
+import { Observable, of } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
@@ -20,5 +20,15 @@ export class PaisesService {
 	getPaisesPorRegion( region: string ): Observable<PaisSmall[]> {
 		const url: string = `${ this.baseUrl }/region/${ region }?fields=alpha3Code,name`
 		return this.http.get<PaisSmall[]>( url );
+	}
+
+	getPaisPorCodigo( codigo: string): Observable<Pais | null> {
+
+		if( !codigo ) {
+			return of(null);
+		}
+
+		const url: string = `${ this.baseUrl }/alpha/${ codigo }`;
+		return this.http.get<Pais>( url );
 	}
 }
